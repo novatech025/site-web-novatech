898b210c6c786cc370e94c4ac43e365049c3beda
KNJSoft <knjprod@gmail.com>
2024-05-24 09:12:29 +0100

    urls modify

--- novatech/urls.py
+++ novatech/urls.py
@@ -30,3 +30,7 @@ urlpatterns = [
     path('contact/', include("contact.urls")),
     path('',include("Nova_tech.urls")),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
